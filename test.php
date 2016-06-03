794f672f6ae30f2d7bfebc21d70848dccba6a1f2
Emiliyan Kadiyski <e.kadiyski@gmail.com>
2016-06-03 14:58:23 +0300

    Emo commit with function

--- test.php
+++ test.php
@@ -1,3 +1,14 @@
 <?php
     echo "this is just a test file! <br>";
     echo "more text!!!";
+
+    echo "Emo text!";
+    public function charge_battery($time){
+		echo "chargin battery...<br/>";
+		echo "stard endurance = $this->battery_power;";
+		$this->battery_power += $time * 10;
+		if($this->battery_power > 100){
+			$this->battery_power = 100;
+		}
+		echo "end endurance = $this->battery_power";
+	}
