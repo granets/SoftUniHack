90d2972bc0879e112d48c8d152e345f4df711c65
granets <mivanchev@gmail.com>
2016-06-04 14:50:20 +0300

    Fixed some bugs on login template

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,7 +9,18 @@ use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() {
         return view('admin');
     }
+
 }
