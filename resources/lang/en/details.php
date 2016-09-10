15fe772d177f0835c6725e2c87371c6f2a1bcf4d
zahariasp <kokonina@abv.bg>
2016-09-10 15:04:15 +0300

    the final push

---
+++ resources/lang/en/details.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'points' => 'Points',
+    'point' => 'points',
+    'all' => 'Total:',
+    'mission' => 'Mission',
+    'comment' => 'Comment',
+    'pnt' => 'p.',
+    'add_points' => 'Add points'
+];
